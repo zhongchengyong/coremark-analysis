4be62640a093ebeec78043e2803d197ed36a09ee
zhongchengyong <zhongcy93@gmail.com>
2024-06-30 16:45:12 +0800

    The coremark score is 3.4 now with unroll-loops

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
-# GCC=/home/zhong/study/Riscv_boards/tina-d1-h/prebuilt/gcc/linux-x86/riscv/toolchain-thead-glibc/riscv64-glibc-gcc-thead_20200702/bin/riscv64-unknown-linux-gnu-gcc
-GCC=/home/zhong/study/Riscv_boards/tina-d1-h/lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528/bin/riscv64-unknown-linux-gnu-gcc
-$GCC -O3 -Irv64 -Iposix -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+GCC=/home/zhong/study/Riscv_boards/tina-d1-h/prebuilt/gcc/linux-x86/riscv/toolchain-thead-glibc/riscv64-glibc-gcc-thead_20200702/bin/riscv64-unknown-linux-gnu-gcc
+
+# GCC=/home/zhong/study/Riscv_boards/tina-d1-h/lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528/bin/riscv64-unknown-linux-gnu-gcc
+
+# Best option
+# $GCC -O3 -Irv64 -Iposix -funroll-all-loops   -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+$GCC -O3 -Irv64 -Iposix -funroll-loops -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+
+# Score 3.1, without unroll-loops
+# $GCC -O3 -Irv64 -Iposix  -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+
+OBJD=/home/zhong/study/Riscv_boards/tina-d1-h/prebuilt/gcc/linux-x86/riscv/toolchain-thead-glibc/riscv64-glibc-gcc-thead_20200702/bin/riscv64-unknown-linux-gnu-objdump
+$OBJD -S coremark.exe > coremark.exe.dis
+$OBJD -S coremark.exe_bak > coremark.exe_bak.dis
+adb push coremark.exe /coremark/
+adb shell /coremark/coremark.exe
---
+++ compiler_analysis.md
@@ -0,0 +1,8 @@
+# Coremark score
+* Compiler:  
+* Dev env: Allwinner D1
+
+
+# Problems
+    1. how the the system call invoked in LLVM target?
+    2. We can see the coremark_bak unroll more loops, why?
