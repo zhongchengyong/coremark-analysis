dbf8ff36da6c70235493f8bdf0c177d7d7b64ce3
zhongchengyong <zhongcy93@gmail.com>
2024-06-30 23:01:22 +0800

    Enhance the build.sh

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 This repo is used to analysis the coremark performance on RISC-V target, which compiled by GCC or LLVM.  
 The compile target will run on the xuantie-C906 core.
 
+# Usage
+* sh build.sh to build coremark with xuantie-gcc
+* sh clang-build.sh to build coremark with clang, which has port the riscv64-clang directory
+
+
 
 The origin coremark repository: https://github.com/eembc/coremark
--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 GCC=/home/zhong/study/Riscv_boards/tina-d1-h/prebuilt/gcc/linux-x86/riscv/toolchain-thead-glibc/riscv64-glibc-gcc-thead_20200702/bin/riscv64-unknown-linux-gnu-gcc
-GCC=clang --target=riscv64 -march=rv64gc --sysroot=/home/zhong/Downloads/riscv/riscv64-unknown-elf --gcc-toolchain=/home/zhong/Downloads/riscv
+# GCC=clang --target=riscv64 -march=rv64gc --sysroot=/home/zhong/Downloads/riscv/riscv64-unknown-elf --gcc-toolchain=/home/zhong/Downloads/riscv
 
 # GCC=/home/zhong/study/Riscv_boards/tina-d1-h/lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528/bin/riscv64-unknown-linux-gnu-gcc
 
 # Best option
-# $GCC -O3 -Irv64 -Iposix -funroll-all-loops   -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
 $GCC -O3 -Irv64 -Iposix -funroll-loops -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
 
-$GCC -O3 -Irv64 -Iposix -funroll-loops -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+# Best option with static
+# $GCC -static -O3 -Irv64 -Iposix -funroll-loops -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
+
 
 # Score 3.1, without unroll-loops
 # $GCC -O3 -Irv64 -Iposix  -I. -DFLAGS_STR=\""-O3 -DPERFORMANCE_RUN=1  -lrt"\"  -DPERFORMANCE_RUN=1 core_list_join.c core_main.c core_matrix.c core_state.c core_util.c posix/core_portme.c -o ./coremark.exe -lrt -DITERATIONS=60000
